9701061e34787bc18e8c08e13cc94f0d80b48a97
Jova <lichanglin163@163.com>
2018-01-06 16:29:49 +0800

    add action function

--- information/admin.py
+++ information/admin.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from .models import *
+from util.util import export_excel
+
 
 # Register your models here.
 
@@ -14,6 +16,13 @@ class StaffInfoAdmin(admin.ModelAdmin):
 
 class BaseInfoAdmin(admin.ModelAdmin):
     list_display = ('name', 'contact_information')
+    actions = ['export_action']
+
+    def export_action(self, request, queryset):
+        export_excel(queryset)
+        self.message_user(request, "export OK!")
+
+    export_action.short_description = "export Excel"
 
 
 class PositionInfoAdmin(admin.ModelAdmin):
---
+++ util/util.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import xlwt
+
+
+def export_excel(sets):
+    print sets
+
